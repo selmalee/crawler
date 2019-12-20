77674d96e2252e498b95c82bdb292f0e41b32c0c
seminelee <747191197@qq.com>
2019-12-21 00:49:18 +0800

    :pencil2:sendmail

--- build/house/result.js
+++ build/house/result.js
@@ -11,10 +11,11 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const email_1 = require("../lib/email");
 const config_1 = require("../config");
-let storageData = {};
+let storageDataHash = {};
+let storageData = [];
 // 定期清理缓存数据
 setInterval(() => {
-    storageData = {};
+    storageDataHash = {};
 }, config_1.STORAGE_TIME);
 /**
  * 展示爬取数据
@@ -22,23 +23,25 @@ setInterval(() => {
  */
 exports.showData = (data, keywords) => __awaiter(void 0, void 0, void 0, function* () {
     // 筛选不在缓存中
-    data = data.filter(item => !storageData[item.text]);
+    data = data.filter(item => !storageDataHash[item.text]);
     // 如果有数据
     if (data.length > 0) {
         console.log(`[${new Date().toLocaleString()}] ${data.length}条记录`);
         // 缓存数据
         data.forEach(item => {
-            storageData[item.text] = true;
+            storageDataHash[item.text] = true;
         });
+        storageData = storageData.concat(data);
         // 凌晨到7点别发邮件了
         if (new Date().getHours() < 7) {
             return;
         }
-        // 防抖函数，延时发送邮件
+        // 发送邮件
         try {
-            const subject = '【豆瓣租房】' + data.length + '条';
-            const text = ' - ' + data.map(item => JSON.stringify(item)).join('\n - ') + '\n\n关键词：' + keywords.toString();
-            const res = yield email_1.sendMail(data, subject, text);
+            const subject = '【豆瓣租房】' + storageData.length + '条';
+            const text = ' - ' + storageData.map(item => JSON.stringify(item)).join('\n - ') + '\n\n关键词：' + keywords.toString();
+            const res = yield email_1.sendMail(storageData, subject, text);
+            storageData = [];
             console.log(`[${new Date().toLocaleString()}] send mail ${res}`);
         }
         catch (e) {
--- src/house/result.ts
+++ src/house/result.ts
@@ -1,10 +1,11 @@
 import { sendMail } from '../lib/email'
 import { STORAGE_TIME } from '../config'
 
-let storageData = {}
+let storageDataHash = {}
+let storageData = []
 // 定期清理缓存数据
 setInterval(() => {
-  storageData = {}
+  storageDataHash = {}
 }, STORAGE_TIME)
 
 /**
@@ -13,23 +14,25 @@ setInterval(() => {
  */
 export const showData = async (data: any[], keywords: RegExp) => {
   // 筛选不在缓存中
-  data = data.filter(item => !storageData[item.text])
+  data = data.filter(item => !storageDataHash[item.text])
   // 如果有数据
   if (data.length > 0) {
     console.log(`[${new Date().toLocaleString()}] ${data.length}条记录`)
     // 缓存数据
     data.forEach(item => {
-      storageData[item.text] = true
+      storageDataHash[item.text] = true
     })
+    storageData = storageData.concat(data)
     // 凌晨到7点别发邮件了
     if (new Date().getHours() < 7) {
       return
     }
-    // 防抖函数，延时发送邮件
+    // 发送邮件
     try {
-      const subject = '【豆瓣租房】' + data.length + '条'
-      const text = ' - ' + data.map(item => JSON.stringify(item)).join('\n - ') + '\n\n关键词：' + keywords.toString()
-      const res = await sendMail(data, subject, text)
+      const subject = '【豆瓣租房】' + storageData.length + '条'
+      const text = ' - ' + storageData.map(item => JSON.stringify(item)).join('\n - ') + '\n\n关键词：' + keywords.toString()
+      const res = await sendMail(storageData, subject, text)
+      storageData = []
       console.log(`[${new Date().toLocaleString()}] send mail ${res}`)
     } catch(e) {
       console.error(`[${new Date().toLocaleString()}] send mail error: ${e.toString()}`)
